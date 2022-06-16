39ee177a9b789a778a32e7fac5623e09517238c6
matheus costa <matheuscosta.tech@gmail.com>
2022-06-15 21:41:00 -0300

    Updating model company

--- companies/models.py
+++ companies/models.py
@@ -3,13 +3,23 @@ from django.utils.timezone import now
 
 
 class Company(models.Model):
+    class Meta:
+        verbose_name_plural = 'Companies'
+
     class CompanyStatus(models.TextChoices):
         LAYOFFS = 'Layoffs'
         HIRING_FREEZE = 'Hiring Freeze'
         HIRING = 'Hiring'
 
     name = models.CharField(max_length=30, unique=True)
-    status = models.CharField(max_length=30, choices=CompanyStatus.choices, default=CompanyStatus.HIRING)
+    status = models.CharField(
+        max_length=30,
+        choices=CompanyStatus.choices,
+        default=CompanyStatus.HIRING
+    )
     last_update = models.DateTimeField(default=now, editable=True)
     notes = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.name
 
