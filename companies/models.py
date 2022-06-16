54853a77b45b9ed12f5e0f150d2cf083ab92aba9
matheus costa <matheuscosta.tech@gmail.com>
2022-06-16 14:17:39 -0300

    New serializer for company

--- companies/models.py
+++ companies/models.py
@@ -4,22 +4,21 @@ from django.utils.timezone import now
 
 class Company(models.Model):
     class Meta:
-        verbose_name_plural = 'Companies'
+        verbose_name_plural = "Companies"
 
     class CompanyStatus(models.TextChoices):
-        LAYOFFS = 'Layoffs'
-        HIRING_FREEZE = 'Hiring Freeze'
-        HIRING = 'Hiring'
+        LAYOFFS = "Layoffs"
+        HIRING_FREEZE = "Hiring Freeze"
+        HIRING = "Hiring"
 
     name = models.CharField(max_length=30, unique=True)
     status = models.CharField(
         max_length=30,
         choices=CompanyStatus.choices,
-        default=CompanyStatus.HIRING
+        default=CompanyStatus.HIRING,
     )
     last_update = models.DateTimeField(default=now, editable=True)
     notes = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
         return self.name
-
