1dae288e9d2b60a46621834a2aa675193f44a988
matheus costa <matheuscosta.tech@gmail.com>
2022-06-16 14:49:31 -0300

    Fixing model field

--- companies/models.py
+++ companies/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import URLField
 from django.utils.timezone import now
 
 
@@ -18,6 +19,7 @@ class Company(models.Model):
         default=CompanyStatus.HIRING,
     )
     last_update = models.DateTimeField(default=now, editable=True)
+    application_link = URLField(blank=True)
     notes = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
