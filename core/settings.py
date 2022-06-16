c008493b3b1715800bf61e9a16efe0b9aebedfa3
matheus costa <matheuscosta.tech@gmail.com>
2022-06-16 14:51:18 -0300

    Basic rest api

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tq8onokk-7mi0n&0px_5b3p9!+m*lyfheym5&o$df!r5y9tx3t'
+SECRET_KEY = (
+    'django-insecure-tq8onokk-7mi0n&0px_5b3p9!+m*lyfheym5&o$df!r5y9tx3t'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'companies',
+    'companies.apps.CompaniesConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
