3ce6c9e924ab4c9a034cf1a54bdd0c4c0b861c98
matheus costa <matheuscosta.tech@gmail.com>
2022-09-06 23:55:07 -0300

    Starting to refactor tests to pytest style

--- companies/test/test_email_service.py
+++ companies/test/test_email_service.py
@@ -1,42 +1,45 @@
 from unittest.mock import patch
 from django.test import TestCase
 from django.core import mail
+import pytest
 
 
-class EmailServiceTests(TestCase):
-    def test_send_email_should_succeed(self):
-        with self.settings(EMAIL_BACKEND='django.core.mail.backends.locmem.EmailBackend'):
-            self.assertEqual(len(mail.outbox), 0)
+def test_send_email_should_succeed(mailoutbox, settings):
+    settings.EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
 
-            mail.send_mail(
-                subject='test subject',
-                message='here is the message',
-                from_email='test@mail.com',
-                recipient_list=['testemail2@mail.com'],
-                fail_silently=False,
-            )
+    assert len(mailoutbox) == 0
 
-            self.assertEqual(len(mail.outbox), 1)
-            self.assertEqual(mail.outbox[0].subject, 'test subject')
+    mail.send_mail(
+        subject='test subject',
+        message='here is the message',
+        from_email='test@mail.com',
+        recipient_list=['testemail2@mail.com'],
+        fail_silently=False,
+    )
 
-    def test_send_email_without_arguments_should_send_empty_email(self):
-        with patch('companies.views.send_mail') as mock_send_mail:
-            res = self.client.post('/send-email/')
+    assert len(mailoutbox) == 1
+    assert mailoutbox[0].subject == 'test subject'
 
-            res_content = res.json()
 
-            self.assertEqual(res.status_code, 200)
-            self.assertEqual(res_content['status'], 'success')
-            self.assertEqual(res_content['info'], 'email sent successfully')
+# def test_send_email_without_arguments_should_send_empty_email(self):
+#     with patch('companies.views.send_mail') as mock_send_mail:
+#         res = self.client.post('/send-email/')
 
-            mock_send_mail.assert_called_with(
-                subject=None,
-                message=None,
-                from_email='thea76@ethereal.email',
-                recipient_list=['thea76@ethereal.email'],
-            )
+#         res_content = res.json()
 
-    def test_send_email_with_get_verb_should_fail(self):
-        res = self.client.get('/send-email/')
-        assert res.status_code == 405
-        assert res.json() == {'detail': 'Method "GET" not allowed.'}
+#         self.assertEqual(res.status_code, 200)
+#         self.assertEqual(res_content['status'], 'success')
+#         self.assertEqual(res_content['info'], 'email sent successfully')
+
+#         mock_send_mail.assert_called_with(
+#             subject=None,
+#             message=None,
+#             from_email='thea76@ethereal.email',
+#             recipient_list=['thea76@ethereal.email'],
+#         )
+
+
+# def test_send_email_with_get_verb_should_fail(self):
+#     res = self.client.get('/send-email/')
+#     assert res.status_code == 405
+#     assert res.json() == {'detail': 'Method "GET" not allowed.'}
